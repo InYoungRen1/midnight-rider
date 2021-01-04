e2ff4b9568fc9fc6adf841eeaf0c9b3ad70910f0
Jack Su <suj052003@gmail.com>
2021-01-04 10:17:37 -0800

    mid night rider

---
+++ main.py
@@ -0,0 +1,23 @@
+# main.py
+# Midnight Rider
+# A text adventure game that is riveting
+# IGN gives 4 stars out of 100
+
+
+
+
+def main():
+    pass
+    # TODO: Show introduction
+
+    # LOOP
+    # TODO: checked if reached END GAME
+
+    # TODO:give player their choices
+
+    # TODO:change the environment based on the choice and random number generator
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
