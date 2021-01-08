bfb47c3f73142c3e1170af1973f2b64e1f50271d
Jack Su <suj052003@gmail.com>
2021-01-08 10:43:26 -0800

    mid night rider updated v4.0

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ INTRODUCTION = """
  
  WELCOME TO MIDNIGHT RIDER
  
- WE'VE STOLEN A CAR. WE NEED TO GET IT HOME.
- THE CAR IS SPECIAL.
+ WE'VE STOLEN A RELIC. WE NEED TO GET IT HOME.
+ THE RELIC IS SPECIAL.
  
- THE GOVERNMENT WANTS IT FOR THEIR GAIN.
+ THE CORP WANTS IT FOR THEIR GAIN.
  
  WE CAN'T LET THAT HAPPEN.
  
- ONE GOAL: SURVIVAL... and THE CAR.
- REACH THE END BEFORE THE MAN GON GETCHU
+ ONE GOAL: SURVIVAL... and THE RELIC.
+ REACH THE END BEFORE THE AGENTS GET YOU.
  
  
  """
@@ -42,23 +42,88 @@ you pressed the button to open the gate.
 This isn't the first time you've done this,
 so you know how to time it exactly.
 Just as the door close, you slide right into the HQ
-You know you did the right thing, the government 
-would have just torn the car apart.
+You know you did the right thing, the corp 
+would have just torn the relic apart.
 They don't know its secrets....
 that is holds the key to different worlds.
-As you step out of the vehicle, Fido runs up to you
+As you step out of the vehicle, Johnny SilverHand runs up to you
 "Thank you for saving me," he says.
 it makes a strange sound.
 It changed its form.
 You've seen it before, but only on TV.
-"...Bumblebee???"
+"...Geralt???"
 "Ruin has come to our family...."
 
 
 ------ Game Over ------
 """
 
+LOSE_HUNGER = """
 
+YOUR STOMACH IS EMPTY 
+WHO KNEW THAT WHAT THE DOCTOR SAID WAS TRUE,
+THAT HUMAN/ROBOT HYBRIDS WOULD NEED 
+DUMPLING TO SUSTAIN THEMSELVES. 
+YOUR ROBOT SYSTEM START TO SHUT DOWN.
+YOUR HUMAN EYES CLOSE.
+THE LAST THING THAT YOU HEAR ARE SIRENS.
+THEY GOT YOU. THEY GOT THE RELIC.
+WE FAILED....
+
+------ GAME OVER ------
+"""
+
+LOSE_AGENTS = """
+
+THE AGENTS HAVE CLOSED IN ON YOU.
+THERE ARE AT LEAST 20 CARS SURROUNDING YOU.
+THE LEAD CAR BUMPS YOUR PASSENGER SIDE. 
+YOU MANAGE TO CORRECT YOUR STEERING
+TO KEEP YOU FROM CRASHING.
+
+YOU DIDN'T SEE THE AGENTS CAR BESIDE YOU. 
+THE DRIVER BUMPS YOUR CAR. 
+AND THAT'S IT.
+
+YOU SPIN OUT OF CONTROL.
+THE CAR FLIPS OVER AT LEAST TWO TIME.
+OR MORE... YOU LOST COUNT.
+
+SIREN.
+
+"ARE THEY ALIVE?" SOMEONE ASKS.
+"DOESN'T MATTER. ALL WE WANTED WAS THE CAR."
+YOU SEE A BLUE EYE MAN WALKING OUR OF THE CAR.
+"WAS IT IN THE CAR THE WHOLE TIME?" YOU 
+THINK TO YOURSELF.
+
+THE BLUE EYE MAN LOOKS UP AT TEH OFFICERS.
+"YOU WILL NEVER THE REVOLUTION."
+"DID THE BLUE EYES JUST TALK?" YOU THINK TO YOURSELF.
+
+YOUR DRIFT OFF INTO UNCONSCIOUSNESS.
+
+------ GAME OVER ------
+"""
+
+LOSE_FUEL = """
+
+THE CAR RUNS OUT OF FUEL,
+THE AGENTS ARE CATCHING UP 
+YOU KNEW YOU HAVE TO DO SOMETHING 
+YOU DECIDED TO DRIVE INTO THE FOREST 
+TO BET ON THE AGENTS COULDN'T FIND YOU
+AS YOU MAKE THE TURNS AND SPED UP
+YOU DIDN'T SEE A TREE BRANCH IN THE WAY
+THE TREE BRANCH PIERCE THROUGH THE WINDSHIELD
+AND STAB RIGHT INTO YOUR EYES AND HEAD
+CAME OUT FROM THE HEAD
+YOU DIDN'T EVEN HAVE TIME TO THINK
+
+YOU DIED
+
+------ GAME OVER ------
+"""
 
 def type_text_output(text):
     for char in textwrap.dedent(text):
@@ -75,9 +140,7 @@ def main():
     MAX_FUEL_LEV = 50
     MAX_DUMPLING = 3
     MAX_DISTANCE_TRAVELLED = 100
-
-    # Debuff from hunger
-    # if hunger == 45:
+    DUMPLING_CHANCE = 0.03
 
 # Variables
     done = False
@@ -92,33 +155,46 @@ def main():
     while not done:
 
         # Fido
-        if random.random() < 0.03:
+        if random.random() < DUMPLING_CHANCE:
             # Fido pops up says something and gives you one dumpling
-            dumplings = MAX_DUMPLING
+            dumplings += 1
             print()
             print("********* You magically gained one dumpling!")
             print("******** \"You're welcome!\" a voice says.")
-            print("********* It's Fido.")
+            print("********* It's Your Ancestor.")
             print("********* He's using his dumpling frying skills to cook you a dumpling.")
-        if hunger > 35:
-            print("********* Gnawing hunger sets in, turning the body against itself, weakening the mind…")
-        elif hunger > 20:
-            print("********* No force of will can overcome a failing body.")
+
         #  checked if reached END GAME
         if km_traveled > MAX_DISTANCE_TRAVELLED:
             # Win
             # print win scenario
             time.sleep(2)
             type_text_output(WIN)
-
-            # Breaking the while loop
             break
-
+        elif hunger > 45:
+            # LOSE - TOO Hungry
+            time.sleep(2)
+            type_text_output(LOSE_HUNGER)
+            break
+        elif agents_distance >= 0:
+            time.sleep(2)
+            type_text_output(LOSE_AGENTS)
+            break
+        elif fuel <= 0:
+            time.sleep(2)
+            type_text_output(LOSE_FUEL)
+            break
+        if hunger > 35:
+                print("********* Gnawing hunger sets in, turning the body against itself, weakening the mind…")
+        elif hunger > 20:
+                print("********* No force of will can overcome a failing body.")
         # give player their choices
         print(CHOICES)
 
         # Handle user's input
         users_choice = input("What do you want to do? ").lower().strip("!,.?")
+        if users_choice == "a","b","c","d","e":
+            turns += 1
 
         if users_choice == "a":
 
@@ -137,7 +213,7 @@ def main():
 
             # Agents distance travelled
             agents_distance_now = (7, 15)
-            agents_distance -= (player_distance_now - agents_distance_now)
+
 
         elif users_choice == "b":
             # Drive
@@ -207,6 +283,7 @@ def main():
 
     # outroduction
     print("Thanks for playing! Please play it again!")
+    print(f"You finished the game in {turns} turns.")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
